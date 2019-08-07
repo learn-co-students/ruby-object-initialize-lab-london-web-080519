718df1bf3be994ac899d1e80eed1ad497d67a926
github username <github email address>
2019-08-07 07:35:54 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,5 @@ class Dog
         @name = name
         @breed = breed
     end
-    # def breed=(breed)
-    #     @breed = "Mutt"
-    # end
-    # def breed
-    #     @breed
-    # end
-    # attr_accessor :name, :breed
     
 end
-mutt = Dog.new("Mut","some breed")
